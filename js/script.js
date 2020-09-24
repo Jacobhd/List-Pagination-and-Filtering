5b335ffeceff6a3e678ac4756d4daf1e1f4463ec
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2020-09-24 10:58:34 -0600

    Revamp
    Currently working on appendPageLinks function
    Redefining arguments
    Cleaning up comments

--- js/script.js
+++ js/script.js
@@ -9,30 +9,22 @@ FSJS project 2 - List Filter and Pagination
 /***
    Add your global variables that store the DOM elements you will
    need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
 ***/
 
 // studentList global variable selects where all students are listed
 const studentList = document.querySelector(".student-list");
-// note studentList is a class
+// note studentList is a class within 'ul' in html
 // console.log(studentList);
 
-// Selects all students
+// Selects all children of the ul (students)
 const pageItems = studentList.children;
 // console.log(pageItems);
+
 /***
    Create the `showPage` function to hide all of the items in the
    list except for the ten you want to show.
 
    Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
      - Remember that a function `parameter` goes in the parens when
        you initially define the function, and it acts as a variable
        or a placeholder to represent the actual function `argument`
@@ -40,22 +32,28 @@ const pageItems = studentList.children;
        "invoke" the function
 ***/
 // storing number of items to show on each page (10), hides the rest
+// named function showPage with two arguments (list, page), with list linking to studentList
 
 const showPage = (list, page) => {
   list = studentList;
-  for (let i = 0; i < list.length; i++)
-    if (i > 10){
+  for (let i = 0; i <= list.length; i++) {
+    if (i >= 10){
       li[i].style.display = "none"
     } else {
       li[i].style.display = "block"
     }
+  }
 }
+
+// showPage(list, 1);
 /*
 loop over items in the list parameter
   if index of list item is >= index of first item that should be shown on page
   && the list item index is <= the index of the last item that
   should be shown on the page, show it
 */
+
+
 /***
    Create the `appendPageLinks function` to generate, append, and add
    functionality to the pagination buttons.
@@ -64,9 +62,11 @@ loop over items in the list parameter
 const appendPageLinks = (list) => {
   list = studentList;
   list.length
-  document.addEventListener
+  document.addEventListener('click', )
+
+
 1  list.length / page max
-2  create div, gove pagination class and append it to the .page div
+2  create div, give pagination class and append it to the .page div
 3  Add a ul to pagination div to store pagination appendPageLinks
    for every page, add li and a tags with page number text
 4  Add an event listener to each a tag. When clicked call showPage function
@@ -76,6 +76,8 @@ const appendPageLinks = (list) => {
 7  Identify that clicked link using event.target
 }
 */
+
+
 
 /* pseudocode:
 global studentList variable
@@ -100,4 +102,3 @@ call append page links function(global variable for list Items)
 
 
 */
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
