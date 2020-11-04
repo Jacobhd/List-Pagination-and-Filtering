d0617ce5a487909cf418166ed82920cdb8e02fb5
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2020-11-04 12:13:01 -0700

    Fixed Errors
    Now targeting node containing 'li' elements. First function loop error.
    Calling the createElement method for the 'li' and 'a' in the for loop within appendPageLinks function.
    Linked totalPages into the for loop and calling showPage function inside the click handler. Errors to fix are remove active class from last 'a' and showing different students in each page.

--- js/script.js
+++ js/script.js
@@ -5,32 +5,29 @@ FSJS project 2 - List Filter and Pagination
 
 
 // Global variables
-const studentList = document.querySelector(".student-list"); //selects '.student-list' element class
+const studentList = document.querySelectorAll(".student-item"); //selects node of 'li' elements containing'students
 console.log(studentList); // test
 
-const pageItems = 10; // Stored max number of items to show on each page
+const pageItems = 10; // Number of students to show on each page
 
 
-//showPage function
+//showPage function to show and hide students
 const showPage = (list, page) => { //list, page parameters to be passed in
-  let startIndex = ( page * pageItems ) - pageItems; //first item
+  let startIndex = ( page * pageItems ) - pageItems; //first item to show
   let endIndex = page * pageItems; //last item
   for (let i = 0; i < list.length; i++) {
-    //currently unexecuted code * currently showing all 'li'
-    if (i >= startIndex && i <= endIndex) {
-      list[i].style.display = 'block'; //show list item index
+    if (i >= startIndex && i < endIndex) {
+      list[i].style.display = 'block'; //show block of 'li' 10 at a time
     } else {
-      list[i].style.display = 'none';
+      list[i].style.display = 'none'; //hides the rest
     }
-  } //*
+  }
 }
 
 
 //appendPageLinks function
 const appendPageLinks = (list) => {
   let totalPages = Math.ceil(list.length / pageItems); //divides total list items by max items on page
-  const li = document.createElement('li'); //creates 'li' element
-  const a = document.createElement('a'); //creates 'anchor' element
   const page = document.querySelector('.page'); //selects '.page' class
   const ul = document.createElement('ul'); //creates 'ul' element
   const div = document.createElement('div'); //creates 'div' element
@@ -38,10 +35,12 @@ const appendPageLinks = (list) => {
   div.appendChild(ul); //adds 'ul' to 'div' to store pagination links
   page.appendChild(div); //appends 'div' to 'page' div
 
-  for (let i = 0; i < pageItems; i++ ) { //for every page, add li and a tags with the page number text
+  for (let i = 1; i < totalPages; i++ ) { //for every page, add li and a tags with the page number text
+    const li = document.createElement('li'); //creates 'li' element
+    const a = document.createElement('a'); //creates 'anchor' element
     a.className = 'active'; //'a' link active
     a.href = '#'; //sets link address
-    a.textContent = 1; //'a' links to start at page 1
+    a.textContent = i; //'a' links to start at page 1
     li.appendChild(a); //adds 'a' to 'li'
     ul.appendChild(li); //adds 'li' to 'ul'
 
@@ -50,9 +49,9 @@ const appendPageLinks = (list) => {
       if (a.target.tagName === 'a')
       a.className.remove = '';
       a.target.className = 'active';
-    }
-  }); //eventListener click on anchor elements
-    showPage(studentList, 1); //2nd argument should be text content of 'a' element that was just clicked
+      showPage(studentList, 1); //2nd argument should be text content of 'a' element that was just clicked
+      }
+    }); //eventListener click on anchor elements
   }
 }
 
